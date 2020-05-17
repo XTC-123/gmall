46d89fcb6d4a7172c006fab4d0907a1f6d528867
xtc_glb <xtc_glb@qq.com>
2020-05-17 18:08:48 +0800

    测试提交

--- gmall-user/src/main/java/com/xtc/gmall/User_Server.java
+++ gmall-user/src/main/java/com/xtc/gmall/User_Server.java
@@ -2,13 +2,12 @@ package com.xtc.gmall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * @author xtc
  * @create 2020-05-17 15:40
  */
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 public class User_Server {
     public static void main(String[] args) {
         SpringApplication.run(User_Server.class, args);
